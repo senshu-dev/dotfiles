bb17737a8bfb1e6545fa7775e253796400df514d
senshu_ <s1kle.important@gmail.com>
2026-09-12 02:59:39 +1000

    nvim: add blink.cmp completion, wire into LSP capabilities

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,8 @@
 return {
 	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
 	config = function()
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		vim.lsp.config("*", { capabilities = capabilities })
 
