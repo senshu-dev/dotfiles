8a114e82aa1fe8fa94bfe90b4f20883176a897ff
senshu_ <s1kle.important@gmail.com>
2026-09-12 02:59:07 +1000

    nvim: wire LSP servers via native vim.lsp.enable

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,44 @@
+return {
+	"neovim/nvim-lspconfig",
+	config = function()
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+		vim.lsp.config("*", { capabilities = capabilities })
+
+		vim.lsp.config("yamlls", {
+			settings = {
+				yaml = {
+					schemaStore = { enable = true },
+					keyOrdering = false,
+				},
+			},
+		})
+
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = { globals = { "vim" } },
+				},
+			},
+		})
+
+		vim.lsp.enable({
+			"gopls",
+			"terraformls",
+			"pyright",
+			"bashls",
+			"yamlls",
+			"dockerls",
+			"helm_ls",
+			"lua_ls",
+			"clangd",
+		})
+
+		vim.diagnostic.config({
+			virtual_text = true,
+			signs = true,
+			underline = true,
+			update_in_insert = false,
+		})
+	end,
+}
